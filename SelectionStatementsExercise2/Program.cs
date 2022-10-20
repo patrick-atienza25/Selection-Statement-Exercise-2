7a4c8b9e18fdf8be128f4d2e008be8543daa2446
patrick atienza <patienza25@gmail.com>
2022-10-19 19:22:29 -0700

    finished exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+            var userInput = Console.ReadLine();
+
+            switch(userInput.ToLower())
+            {
+                case "science":
+                    Console.WriteLine("I love bill nye");
+                    break;
+                case "math":
+                    Console.WriteLine("I love Pi");
+                    break;
+                case "physical education":
+                    Console.WriteLine("I run after twinkies");
+                    break;
+                case "spanish":
+                    Console.WriteLine("I love tacos");
+                    break;
+                case "history":
+                    Console.WriteLine("I love hercules");
+                    break;
+                default:
+                    Console.WriteLine("I do not know of that class");
+                    break;
+            }
         }
     }
 }
